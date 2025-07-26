bd8171c22746d4d7ac589efa87cefa783d4b9439
Raslan <Raslan@gmail.com>
2025-07-26 20:08:43 +0300

    Init

---
+++ Add.js
@@ -0,0 +1,139 @@
+
+let expenses = JSON.parse(localStorage.getItem("expenses") || "[]");
+
+let balance = Number(localStorage.getItem("balance"));
+let income = Number(localStorage.getItem("income"));
+let expense = Number(localStorage.getItem("expense"));
+
+
+let name, categories, amount;
+
+
+document.addEventListener("DOMContentLoaded", event =>{
+
+        name = document.getElementById("name");
+        categories = document.getElementById("categories");
+        amount = document.getElementById("amount");
+        LoadCategories();
+        
+       const addBtn = document.getElementById("addExpense");
+       const isEditMode = localStorage.getItem("editMode") === "true";
+       if(isEditMode){
+          localStorage.removeItem("editMode");
+          let expenseIndex = Number(localStorage.getItem("expenseToEditIndex"));
+          localStorage.removeItem("expenseToEditIndex");
+          loadExpense(expenseIndex);
+          addBtn.onclick = () => EditExpense(expenseIndex);
+       } 
+
+      else{
+       addBtn.onclick = addExpense;
+       }
+});
+
+function loadExpense(expenseIndex){
+        name.value = expenses[expenseIndex].name;
+        categories.value = expenses[expenseIndex].category;
+        amount.value = expenses[expenseIndex].amount;
+}
+
+
+function ValidateExpense(){
+      let expenseName = name.value;
+      let category = categories.value;
+      let amountValue = amount.value;
+      
+      if(category == "Select Category" || amountValue == "" || expenseName == ""){
+        MakeMessage("Please fill all the data required!", "red");
+        return false;
+      }
+      else{
+        amountValue = Number(amountValue);
+        if(amountValue < 0){
+          MakeMessage("Amount Can not be Negative!", "red");
+          return false;
+        }
+      }
+      return true;     
+}
+function EditExpense(expenseIndex){
+        event.preventDefault();
+        const validExpense = ValidateExpense();
+        if(validExpense){
+        const oldAmount = Number(expenses[expenseIndex].amount);
+
+        expenses[expenseIndex].name = name.value;
+        expenses[expenseIndex].cateogry = categories.value;
+        expenses[expenseIndex].amount = amount.value;
+        const newAmount = Number(expenses[expenseIndex].amount);
+        localStorage.setItem("expenses", JSON.stringify(expenses));
+
+
+       balance += oldAmount;
+       balance -= newAmount;
+       localStorage.setItem("balance", balance);
+
+       expense -= oldAmount;
+       expense += newAmount;
+       localStorage.setItem("expense", expense);
+      
+        MakeMessage("✅ Expense Edited successfully!", "green");
+        }
+}
+
+function LoadCategories(){
+  const categoriesElement =  document.getElementById("categories");
+      const cateogries = localStorage.getItem("cateogries").split(",");
+      console.log(cateogries);
+       cateogries.forEach(cateogry => {
+         let option = document.createElement("option");
+         option.textContent = cateogry;   
+         categoriesElement.appendChild(option);
+       }); 
+}
+
+
+function MakeMessage(messageText, messageColor){
+         const message = document.getElementById("message");
+         message.classList = "";
+  
+          message.classList.add("visible");
+          message.classList.add(`${messageColor}Message`);
+          message.textContent = messageText;
+
+          setTimeout(() => {
+          message.classList.remove("visible");
+          message.classList.add("hidden");
+         }, 2000)
+      
+}
+function addExpense(){
+      event.preventDefault();
+      const validExpense = ValidateExpense();
+
+      if(validExpense){
+      let date = Date();
+      let newExpense = {
+        name: name.value,
+        category: categories.value,
+        amount: amount.value,
+        date: date
+      };
+
+      expenses.push(newExpense);
+      localStorage.setItem("expenses", JSON.stringify(expenses));
+      let amountValue = Number(amount.value);
+      balance -= amountValue;
+      localStorage.setItem("balance", balance);
+
+      expense += amountValue;
+      localStorage.setItem("expense", expense);
+
+
+      MakeMessage("✅ Expense added successfully!", "green");
+      name.value = "";
+      categories.value = "Select Category";
+      amount.value = "";
+    }
+}
+
---
+++ Index.js
@@ -0,0 +1,170 @@
+
+
+let expenses = JSON.parse(localStorage.getItem("expenses") || "[]");
+
+let balance = Number(localStorage.getItem("balance"));
+let income = Number(localStorage.getItem("income"));
+let expense = Number(localStorage.getItem("expense"));
+
+document.addEventListener("DOMContentLoaded", event => {
+    RenderUserData();
+
+    const expenseInterval = document.getElementById("expenseInterval");
+
+    let interval = localStorage.getItem("selectedInterval") || "All";
+    expenseInterval.value = interval;
+    let {first, last} = getExpenses(interval);
+    RenderExpenses(first, last);
+
+
+    expenseInterval.addEventListener("change", function(){
+        interval = expenseInterval.value;
+        localStorage.setItem("selectedInterval", interval);
+        const {first, last} = getExpenses(interval);
+        RenderExpenses(first, last);
+    });
+
+    const incomeBtn = document.getElementById("income");
+
+    incomeBtn.onclick = function (){
+        let message = "Enter Your Income:";
+        let userInput;
+        let incomeInput;
+        while(true){
+            userInput = prompt(`${message}`);
+            if(userInput === null)
+                return;
+
+            incomeInput = Number(userInput);
+            if(!isNaN(incomeInput)){
+                break;
+            }
+            message = "Invalid Input\nEnter Your Income:";
+        }
+
+        income = incomeInput;
+        balance = income - expense;
+        
+         localStorage.setItem("income", income);
+         localStorage.setItem("balance", balance);
+         RenderUserData();
+
+    }
+});
+
+
+function isSameDay(d1, d2){
+    return (d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate());
+}
+
+function isSameMonth(d1, d2){
+    return (d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth());
+}
+
+
+function getExpenses(interval){
+        console.log(interval);
+        let first = -1, last = -1;
+        const currentDate = new Date();
+        if(interval === "Today"){
+            for(let i = expenses.length - 1 ; i >= 0 ; i--){
+                let expenseDate = new Date(expenses[i].date);
+                if(last === -1 && isSameDay(expenseDate, currentDate))
+                    last = i;
+                if(isSameDay(expenseDate, currentDate))
+                    first = i;
+                else
+                    break;
+            }
+        }
+        else if(interval == "Month"){
+             for(let i = expenses.length - 1 ; i >= 0 ; i--){
+                let expenseDate = new Date(expenses[i].date);
+                if(last === -1 && isSameMonth(expenseDate, currentDate))
+                    last = i;
+                if(isSameMonth(expenseDate, currentDate))
+                    first = i;
+                else
+                    break;
+            }   
+        }
+        else{
+            first = 0;
+            last = expenses.length - 1;
+        }  
+
+        return {first, last};
+    }
+
+
+function RenderExpenses(first, last){
+    const expenseContainer = document.getElementById("expenseList");
+    expenseContainer.innerHTML = "";
+    for(let i = last; i >= first ; i--){
+        let exp = expenses[i];
+        const div = document.createElement("div");
+        div.classList = "expenseItem";
+
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        editBtn.classList = "RecordBtn";
+        editBtn.onclick = () => EditExpense(i);
+
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList = "RecordBtn";
+        deleteBtn.onclick = () => DeleteExpense(i);
+
+        div.innerHTML = `
+        <span>${exp.name}</span>
+        <span>${exp.category}</span>
+        <span>${exp.amount}</span>`;
+        div.appendChild(editBtn);
+        div.appendChild(deleteBtn);
+
+        expenseContainer.appendChild(div);
+    }
+    
+}
+
+async function RenderUserData(){
+    
+        const balanceElement = document.getElementById("balance");
+        const incomeElement = document.getElementById("income");
+        const expenseElement = document.getElementById("expense");
+
+        
+        balanceElement.textContent = `Balance: ${balance}$`;
+        incomeElement.textContent = `Income: ${income}$`;
+        expenseElement.textContent = `Expense: ${expense}$`;
+    
+}
+
+
+
+
+function DeleteExpense(index){
+    
+    const balanceElement = document.getElementById("balance");
+    const incomeElement = document.getElementById("income");
+    let amount = expenses[index].amount;
+
+    balance += Number(amount);
+    expense -= Number(amount);
+
+    localStorage.setItem("balance", balance);
+    localStorage.setItem("expense", expense);
+
+    expenses.splice(index, 1);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    location.reload();
+}
+
+
+function EditExpense(index){
+    localStorage.setItem("expenseToEditIndex", index);
+    localStorage.setItem("editMode", "true");
+
+    window.location.href = "Add.html";
+}
