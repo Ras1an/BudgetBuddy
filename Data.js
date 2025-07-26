94c755038d87947dbe25ea0b7fc0cf747ed19a15
Mostafa Omar <mostafaomar112002@gmail.com>
2025-07-26 20:14:16 +0300

    Delete Data.js

--- Data.js
+++
@@ -1,9 +0,0 @@
- let cateogries = ["Food", "Rent", "Education", "Car", "Entertainment", "Others"];
- let balance = 5000;
- let income = 5000;
- let expense = 0;
-
-localStorage.setItem("balance", balance);
-localStorage.setItem("income", income);
-localStorage.setItem("expense", expense);
-localStorage.setItem("cateogries", cateogries);
